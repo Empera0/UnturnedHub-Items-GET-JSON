934a6bf33a2fe84764e8e91f6ffd43e7a523a3a9
Oğuz Görencan <63954402+Empera0@users.noreply.github.com>
2023-03-26 16:06:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -20,46 +20,53 @@ with open("D:\jsonfile\items.json", "r") as f:
     data = json.load(f)
 
 for name in fixed_item_names:
-    
     item_url = f"https://unturnedhub.com/item/{name}"
     item_response = requests.get(item_url)
     item_tree = html.fromstring(item_response.text)
-    
-    item_id = item_tree.xpath('//td[contains(text(), "Item ID")]/following-sibling::td/text()')[0]
-    weight = item_tree.xpath('//td[contains(text(), "Weight")]/following-sibling::td/text()')[0]
-    item_type = item_tree.xpath('//td[contains(text(), "Type")]/following-sibling::td/a/span/text()')[0]
-    rarity = item_tree.xpath('//td[contains(text(), "Rarity")]/following-sibling::td/a/span/text()')[0]
-    horizontal_slots = item_tree.xpath('//td[contains(text(), "Horizontal Slots")]/following-sibling::td/text()')[0]
-    vertical_slots = item_tree.xpath('//td[contains(text(), "Vertical Slots")]/following-sibling::td/text()')[0]
-    
+
     try:
-        description = item_tree.xpath('//p/text()')[3]
+        item_id = item_tree.xpath('//td[contains(text(), "Item ID")]/following-sibling::td/text()')[0]
         pass
     except Exception as e:
-        description = None
-        print(name + "BU EŞYANIN AÇIKLAMA KISMI YÜKLENMEDİ --------------------------------------------") 
-    
+        item_id = None
+        print(e) 
+        pass
     try:
-        magazine = item_tree.xpath('//td[contains(text(), "Magazine")]/following-sibling::td/a/text()')[0]
+        rarity = item_tree.xpath('//td[contains(text(), "Rarity")]/following-sibling::td/a/span/text()')[0]
         pass
     except Exception as e:
-        magazine = None
+        rarity = None
+        print(e)
         pass
     
-    data.setdefault('item',[]).append({
+
+    
+
+    IDrarity = 0    
+    match rarity:
+        case "Common":
+            IDrarity = 1
+        case "Uncommon":
+            IDrarity = 2
+        case "Rare":
+            IDrarity = 3
+        case "Epic":
+            IDrarity = 4
+        case "Legendary":
+            IDrarity = 5
+        case "Mythical":
+            IDrarity = 6
+            
+
+    data[item_id] = []
+    data[item_id].append({
         "name": name,
-        "item_id": item_id,
-        "weight": weight,
-        "item_type": item_type,
-        "rarity": rarity,
-        "horizontal_slots": horizontal_slots,
-        "vertical_slots": vertical_slots,
-        "description": description,
-        "magazine": magazine,
-        "image_url": f"https://unturnedhub.com/img/item/200/{name}.png"})
+        "photo": f"https://unturnedhub.com/img/item/500/{name}.png",
+        "chance": IDrarity,
+        "rarity": IDrarity})
     
-    with open("items.json", "w") as f: # YOU CAN CHANGE LOCATION
-        json.dump(data, f)
+    with open("D:\jsonfile\items.json", "w") as f: # YOU CAN CHANG LOCATION
+        json.dump(dict(data), f)
     print(name +" ataması yapıldı")
 
 
